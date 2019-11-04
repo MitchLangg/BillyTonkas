a9b5178ffbd47dd6c52ea6ed0baf7fb394709a75
Izacc Lucas <Izacc.casey-lucas01@stclairconnect.ca>
2019-11-04 13:48:06 -0500

    #11 Completed Added menu bar Added File and Tools menus to menubar Added Exit menuItem under File Added Exit button functionality I.e program closes after click

--- src/panes/HomePage.java
+++ src/panes/HomePage.java
@@ -1,7 +1,52 @@
 package panes;
 
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
-public class HomePage extends Pane{
-
+public class HomePage extends BorderPane{
+	public HomePage() {
+		//Creates the Menu bar
+		MenuBar menuBar = new MenuBar();
+		
+		
+		//MENU BAR PAGES -------------------------------------------
+		
+		//Creates the drop down menu "File" in the menu bar
+		Menu fileMenu = new Menu("File");
+		Menu toolsMenu = new Menu("Tools");
+		
+		
+		//----------------------------------------------------------
+		
+		//MENU BAR ITEMS -------------------------------------------
+		
+		//Creates the menu item "Exit"
+		MenuItem fileMenu1 = new MenuItem("Exit");
+		
+		
+		
+		//Adds the exit menu item to File
+		fileMenu.getItems().add(fileMenu1);
+		
+		//Adds all menubar pages and their children to the menubar
+		menuBar.getMenus().addAll(fileMenu, toolsMenu);
+		
+		//----------------------------------------------------------
+		
+		
+		//MENU BAR FUNCTIONS ----------------------------------
+		
+		//When the exit button is clicked the program is closed
+		fileMenu1.setOnAction(e->{
+			System.exit(0);
+		});
+		
+		
+		//-----------------------------------------------------
+		//Sets the menu bar  to top (displays it to screen)
+		this.setTop(menuBar);
+	}
 }
