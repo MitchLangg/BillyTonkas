090c04d31483363fff032e82484e1b9eb89d83ca
Mitch <mitchlang444@gmail.com>
2019-11-08 15:45:07 -0500

    Created Database Connection

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,11 +1,28 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Database {
 
 	private static Database instance = null;
-	private Connection connection = null;
+	public static Connection connection;
 	
 	
+	private Database() {
+		if(connection == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager.getConnection("jdbc:mysql://php.scweb.ca/" + Const.DB_NAME, 
+						Const.DB_USER, Const.DB_PASS);
+				System.out.println("Created Connection");
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
+
