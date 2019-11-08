fa047efdc44c17f465acceabb279b55df32a7b5a
Mitch <mitchlang444@gmail.com>
2019-11-08 16:19:32 -0500

    Creation of Javabeans Login class

--- src/javabeans/Inventory.java
+++ src/javabeans/Inventory.java
@@ -6,30 +6,27 @@ package javabeans;
  */
 public class Inventory {
 
-	public int id;
-	public String name;
-	
+	private int id;
+	private String name;
 	public Inventory(int id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
-
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	
 	
 }
