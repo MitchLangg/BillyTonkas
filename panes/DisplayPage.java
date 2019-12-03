3e7374f30943874e21e89e985d20d5668f3be670
Izacc Lucas <Izacc.casey-lucas01@stclairconnect.ca>
2019-12-03 16:59:47 -0500

    Added more stylization to the display page Fully Functional

--- panes/DisplayPage.java
+++ panes/DisplayPage.java
@@ -7,13 +7,19 @@ import javax.swing.JTable;
 
 import javafx.embed.swing.SwingNode;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import main.MainRun;
 import scenes.AddScene;
@@ -32,6 +38,57 @@ public class DisplayPage extends BorderPane{
     	this.setBackground(rootBackground);
     	java.awt.Color tan = new java.awt.Color(210, 180, 140);
     	
+    	 //Submit Button Borders/Backgrounds/Font
+        Background buttonBackground = new Background(
+				new BackgroundFill(Color.MAROON, new CornerRadii(10), new Insets(0, 0, 0, 0)));
+		
+		Border buttonBorder = new Border(
+				new BorderStroke(Color.BROWN, BorderStrokeStyle.SOLID, new CornerRadii(10), new BorderWidths(2)));
+		
+		Background buttonBackgroundHover = new Background(
+				new BackgroundFill(Color.RED, new CornerRadii(10), new Insets(0, 0, 0, 0)));
+		
+		Border buttonBorderHover = new Border(
+				new BorderStroke(Color.CHOCOLATE, BorderStrokeStyle.SOLID, new CornerRadii(10), new BorderWidths(2)));
+		//button font
+		Font buttonFont = Font.font("Ariel", 18);
+    	
+    	Text candyButton = new Text("Candy Table");
+    	candyButton.setFont(buttonFont);
+    	candyButton.setFill(Color.WHITE);
+		VBox candyButtonBox = new VBox();
+		candyButtonBox.getChildren().add(candyButton);
+		candyButtonBox.setMaxHeight(80);
+		candyButtonBox.setMaxWidth(200);
+		candyButtonBox.setAlignment(Pos.CENTER);
+		candyButtonBox.setSpacing(25);
+		candyButtonBox.setBackground(buttonBackground);
+		candyButtonBox.setBorder(buttonBorder);
+		
+		//Delete Button
+        Text gummyButton = new Text("Gummy Table");
+        gummyButton.setFont(buttonFont);
+        gummyButton.setFill(Color.WHITE);
+		VBox gummyButtonBox = new VBox();
+		gummyButtonBox.getChildren().add(gummyButton);
+		gummyButtonBox.setMaxHeight(80);
+		gummyButtonBox.setMaxWidth(200);
+		gummyButtonBox.setAlignment(Pos.CENTER);
+		gummyButtonBox.setSpacing(25);
+		gummyButtonBox.setBackground(buttonBackground);
+		gummyButtonBox.setBorder(buttonBorder);
+		
+		Text chocolateButton = new Text("Chocolate Button");
+		chocolateButton.setFont(buttonFont);
+		chocolateButton.setFill(Color.WHITE);
+		VBox chocolateButtonBox = new VBox();
+		chocolateButtonBox.getChildren().add(chocolateButton);
+		chocolateButtonBox.setMaxHeight(80);
+		chocolateButtonBox.setMaxWidth(200);
+		chocolateButtonBox.setAlignment(Pos.CENTER);
+		chocolateButtonBox.setSpacing(25);
+		chocolateButtonBox.setBackground(buttonBackground);
+		chocolateButtonBox.setBorder(buttonBorder);
  		
     	//Contents of the page
     	String[] candyColumnNames = {"id",
@@ -82,9 +139,6 @@ public class DisplayPage extends BorderPane{
     	scrollPane.getViewport().setBackground(tan);
     	swingNode.setContent(scrollPane);
     	
-    	Button candyButton = new Button("Candy Table");
-    	Button gummyButton = new Button("Gummy Table");
-    	Button chocolateButton = new Button("Chocolate Table");
     	
  		//MENU BAR FUNCTIONS
  		//When the exit button is clicked the program is closed
@@ -111,26 +165,57 @@ public class DisplayPage extends BorderPane{
  			ConnectPage.checkbox.setSelected(true);
  			ConnectPage.checkbox.setDisable(true);
  		});
- 		candyButton.setOnAction(e->{
+ 		candyButtonBox.setOnMouseEntered(e -> {
+ 			candyButtonBox.setBackground(buttonBackgroundHover);
+			candyButtonBox.setBorder(buttonBorderHover);
+		});
+ 		candyButtonBox.setOnMouseExited(e -> {
+ 			candyButtonBox.setBackground(buttonBackground);
+ 			candyButtonBox.setBorder(buttonBorder);
+		});
+ 		candyButtonBox.setOnMouseClicked(e->{
  			JScrollPane scrollerPane = new JScrollPane(candyTableInfo);
+ 			scrollerPane.getViewport().setBackground(tan);
  	    	swingNode.setContent(scrollerPane);
+ 	    	
  		});
-		gummyButton.setOnAction(e->{
+ 		gummyButtonBox.setOnMouseEntered(e -> {
+ 			gummyButtonBox.setBackground(buttonBackgroundHover);
+ 			gummyButtonBox.setBorder(buttonBorderHover);
+		});
+ 		gummyButtonBox.setOnMouseExited(e -> {
+ 			gummyButtonBox.setBackground(buttonBackground);
+ 			gummyButtonBox.setBorder(buttonBorder);
+		});
+		gummyButtonBox.setOnMouseClicked(e->{
 			JScrollPane scrollerPane = new JScrollPane(gummyTableInfo);
+			scrollerPane.getViewport().setBackground(tan);
 	    	swingNode.setContent(scrollerPane);		
 		});
-		chocolateButton.setOnAction(e->{
+		chocolateButtonBox.setOnMouseEntered(e -> {
+			chocolateButtonBox.setBackground(buttonBackgroundHover);
+ 			chocolateButtonBox.setBorder(buttonBorderHover);
+		});
+		chocolateButtonBox.setOnMouseExited(e -> {
+ 			chocolateButtonBox.setBackground(buttonBackground);
+ 			chocolateButtonBox.setBorder(buttonBorder);
+		});
+		
+		chocolateButtonBox.setOnMouseClicked(e->{
 			JScrollPane scrollerPane = new JScrollPane(chocolateTableInfo);
+			scrollerPane.getViewport().setBackground(tan);
 	    	swingNode.setContent(scrollerPane);	
 		});
+		
  		//Sets the menu bar  to top (displays it to screen)
  		this.setTop(MainMenuBar.getMenuBar());
  		
  		VBox vbox = new VBox();
- 		VBox.setMargin(candyButton, new Insets(100,0,100,0));
- 		VBox.setMargin(gummyButton, new Insets(100,0,100,0));
- 		VBox.setMargin(chocolateButton, new Insets(100,0,100,0));
- 		vbox.getChildren().addAll(candyButton, gummyButton, chocolateButton);
+ 		VBox.setMargin(candyButton, new Insets(5,0,5,0));
+ 		VBox.setMargin(gummyButton, new Insets(5,0,5,0));
+ 		VBox.setMargin(chocolateButton, new Insets(5,0,5,0));
+ 		vbox.getChildren().addAll(candyButtonBox, gummyButtonBox, chocolateButtonBox);
+ 		vbox.setSpacing(5);
  		this.setRight(vbox);
  		//Layout of content of page
  		this.setCenter(swingNode);
